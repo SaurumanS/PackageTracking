408bb32515d8ac6ea59190aa5049b5f6b65fd158
SaurumanS <SaurumanS@DESKTOP-PNLAJRA>
2019-03-29 12:03:26 +0300

    29.03.19(1)

--- PackageTracking/PackageTracking/DataAboutEnteredParcels.xaml.cs
+++ PackageTracking/PackageTracking/DataAboutEnteredParcels.xaml.cs
@@ -16,17 +16,31 @@ namespace PackageTracking
 		public DataAboutEnteredParcels ()
 		{
 			InitializeComponent ();
+
+            //var d = new List<RussianPostClassLibrary.ParcelDescription>
+            //{
+            //    new RussianPostClassLibrary.ParcelDescription{Barcode="54645",ProcessStatus=true },
+            //    new RussianPostClassLibrary.ParcelDescription{Barcode="54665",ProcessStatus=true},
+            //    new RussianPostClassLibrary.ParcelDescription{Barcode="5465",ProcessStatus=false },
+            //    new RussianPostClassLibrary.ParcelDescription{Barcode="545",ProcessStatus=true }
+            //};
+            //var groups = d.Where(x => x.ProcessStatus == true).GroupBy(x => x.StatusParcel).Select(g => new GroupingParcels<string, RussianPostClassLibrary.ParcelDescription>(g.Key, g));
+            //foreach (var item in groups)
+            //{
+            //    Parcels.Add(new GroupingParcels<string, RussianPostClassLibrary.ParcelDescription>("DD", item));
+            //}
             Parcels = new ObservableCollection<GroupingParcels<string, RussianPostClassLibrary.ParcelDescription>>();
             this.BindingContext = this;
 		}
-        
+       
+
         public ObservableCollection<GroupingParcels<string,RussianPostClassLibrary.ParcelDescription>> Parcels { get; set; }
         public void AddNewParcels(RussianPostClassLibrary.ParcelDescription[] descriptions)
         {
             var groups = descriptions.Where(x=> x.ProcessStatus==true).GroupBy(x => x.StatusParcel).Select(g => new GroupingParcels<string, RussianPostClassLibrary.ParcelDescription>(g.Key, g));
             foreach(var item in groups)
             {
-                Parcels.Add(item);
+                Parcels.Add(new GroupingParcels<string, RussianPostClassLibrary.ParcelDescription>(item.CurrentlyStatusParcel,item));
             }
         }
     }
